0caa9e344e0c24df276232f123cc918da8823492
Oscar J. Rodriguez B <josuer08@gmail.com>
2020-03-18 22:06:46 -0400

    Major changes in the navbar and start of a parallax

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Oscar J Rodriguez B' });
+  res.render('index', { title: 'Oscar Rodríguez' });
+});
+
+router.get('/plantilla', function(req, res, next) {
+  res.render('plantilla');
 });
 
 module.exports = router;
